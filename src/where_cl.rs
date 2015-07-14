1a3b68f4fe53697588c0838c6d520c28ae53e0ea
defyrlt <defyrlt@gmail.com>
2015-07-15 02:12:52 +0300

    Removed ?Sized, added impl for &Where and test for it

--- src/where_cl.rs
+++ src/where_cl.rs
@@ -16,18 +16,18 @@ trait ToSQL {
     fn to_sql(&self) -> String;
 }
 
-struct Where<'a, T: 'a + ?Sized> {
+struct Where<'a, T: 'a> {
     pub operator: Operator,
-    pub clause: &'a [&'a T]
+    pub clause: &'a [T]
 }
 
-impl ToSQL for str {
+impl<'a> ToSQL for &'a str {
     fn to_sql(&self) -> String {
         self.to_string()
     }
 }
 
-impl<'a, T: ToSQL + ?Sized> ToSQL for Where<'a, T> {
+impl<'a, T: ToSQL> ToSQL for Where<'a, T> {
     fn to_sql(&self) -> String {
         let operator = &format!(" {} ", self.operator.to_sql());
         let mut rv = String::new();
@@ -38,6 +38,12 @@ impl<'a, T: ToSQL + ?Sized> ToSQL for Where<'a, T> {
                     .join(operator));
         rv.push(')');
         rv
+    }
+}
+
+impl<'a, T: ToSQL> ToSQL for &'a Where<'a, T>{ 
+    fn to_sql(&self) -> String {
+        (**self).to_sql()
     }
 }
 
@@ -61,5 +67,55 @@ mod tests {
             clause: &["foo=bar", "fizz=bazz"]
         };
         assert_eq!(foo.to_sql(), "(foo=bar AND fizz=bazz".to_string())
+    }
+
+    #[test]
+    fn test_nested_where_clauses() {
+        let foo = Where {
+            operator: Operator::And,
+            clause: &["foo == bar", "fizz == bazz"]
+        };
+
+        let bar = Where {
+            operator: Operator::And,
+            clause: &["a == b", "c == d"]
+        };
+
+        let bazz = Where {
+            operator: Operator::Or,
+            clause: &[&foo, &bar]
+        };
+
+        assert_eq!(bazz.to_sql(), "(foo == bar AND fizz == bazz) OR (a == b AND c == d)".to_string());
+    }
+
+    #[test]
+    fn test_really_nested_where_clauses() {
+        let foo = Where {
+            operator: Operator::And,
+            clause: &["foo == bar", "fizz == bazz"]
+        };
+
+        let bar = Where {
+            operator: Operator::And,
+            clause: &["a == b", "c == d"]
+        };
+
+        let bazz1 = Where {
+            operator: Operator::Or,
+            clause: &[&foo, &bar]
+        };
+
+        let bazz2 = Where {
+            operator: Operator::Or,
+            clause: &[&bar, &foo]
+        };
+
+        let fizz = Where {
+            operator: Operator::And,
+            clause: &[&bazz1, &bazz2]
+        };
+
+        assert_eq!(fizz.to_sql(), "(((foo == bar AND fizz == bazz) OR (a == b AND c == d)) AND ((a == b AND c == d) OR (foo == bar AND fizz == bazz)))".to_string());
     }
 }
